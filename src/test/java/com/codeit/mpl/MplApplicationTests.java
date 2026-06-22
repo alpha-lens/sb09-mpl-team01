b9ac10aad2bd569ca1c986fbcb1bcf37a562dccd
alpha_lens <mathematic026@gmail.com>
2026-06-22 12:39:30 +0900

    init: 모두의 플리

---
+++ src/test/java/com/codeit/mpl/MplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.mpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MplApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
