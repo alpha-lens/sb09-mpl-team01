ab7d8e5bfbb4c6f73b112829c9fdfc926ae406a9
alpha_lens <mathematic026@gmail.com>
2026-06-26 13:11:47 +0900

    Refactor: import 누락 추가

--- src/main/java/com/codeit/mpl/domain/notification/entity/Notification.java
+++ src/main/java/com/codeit/mpl/domain/notification/entity/Notification.java
@@ -1,6 +1,7 @@
 package com.codeit.mpl.domain.notification.entity;
 
 import com.codeit.mpl.infra.common.entity.base.BaseEntity;
+import com.codeit.mpl.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
