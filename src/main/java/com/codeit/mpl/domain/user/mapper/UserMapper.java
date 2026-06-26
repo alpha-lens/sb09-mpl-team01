960a701e841db6037d238df484fc9eae2b62f7cd
geumbi <gkrry0404@naver.com>
2026-06-26 16:11:57 +0900

    refactor: UserMapper를 MapStruct 인터페이스로 변경

--- src/main/java/com/codeit/mpl/domain/user/mapper/UserMapper.java
+++ src/main/java/com/codeit/mpl/domain/user/mapper/UserMapper.java
@@ -3,28 +3,14 @@ package com.codeit.mpl.domain.user.mapper;
 import com.codeit.mpl.domain.user.dto.response.UserDto;
 import com.codeit.mpl.domain.user.dto.response.UserSummary;
 import com.codeit.mpl.domain.user.entity.User;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Component
-public class UserMapper {
+@Mapper(componentModel = "spring")
+public interface UserMapper {
 
-    public UserDto toDto(User user) {
-        return new UserDto(
-                user.getId(),
-                user.getCreatedAt(),
-                user.getEmail(),
-                user.getName(),
-                user.getProfileImageUrl(),
-                user.getRole(),
-                user.isLocked()
-        );
-    }
+    UserDto toDto(User user);
 
-    public UserSummary toSummary(User user) {
-        return new UserSummary(
-                user.getId(),
-                user.getName(),
-                user.getProfileImageUrl()
-        );
-    }
+    @Mapping(source = "id", target = "userId")
+    UserSummary toSummary(User user);
 }
