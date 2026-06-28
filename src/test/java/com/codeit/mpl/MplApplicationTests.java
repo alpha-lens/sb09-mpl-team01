2063ec321df0eb07ec5e04cee53b03fb11c79a60
Alpha <mathematic026@gmail.com>
2026-06-29 08:24:38 +0900

    Refactor: 개발 DB를 supabase로 변경

--- src/test/java/com/codeit/mpl/MplApplicationTests.java
+++ src/test/java/com/codeit/mpl/MplApplicationTests.java
@@ -3,6 +3,9 @@ package com.codeit.mpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class MplApplicationTests {
 
