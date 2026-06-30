2a448e2cf0dde0028f92402f3f8a4730159c0a9c
Alpha <mathematic026@gmail.com>
2026-06-30 22:46:39 +0900

    Fix: EnableJpaAuditing을 추가하여 createdAt 자동으로 들어가게 설정

--- src/main/java/com/codeit/mpl/MplApplication.java
+++ src/main/java/com/codeit/mpl/MplApplication.java
@@ -4,9 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableJpaAuditing
 public class MplApplication {
 
   public static void main(String[] args) {
