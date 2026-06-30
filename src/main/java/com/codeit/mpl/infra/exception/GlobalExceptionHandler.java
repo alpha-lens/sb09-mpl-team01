3569dc215c8ea1258e5ad8e85d7530a3ecf4f13f
Alpha <mathematic026@gmail.com>
2026-06-30 22:46:03 +0900

    Refactor: exception 로깅

--- src/main/java/com/codeit/mpl/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codeit/mpl/infra/exception/GlobalExceptionHandler.java
@@ -2,12 +2,14 @@ package com.codeit.mpl.infra.exception;
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -33,6 +35,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unhandled exception occurred: ", e);
         return ResponseEntity
                 .internalServerError()
                 .body(new ErrorResponse("InternalServerError", "서버 오류가 발생했습니다.", null));
