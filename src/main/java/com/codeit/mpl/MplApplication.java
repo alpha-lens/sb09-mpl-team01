308c057797043fd7609b7ddf001f5d99c860f117
sungho <parksh3070@daum.net>
2026-07-01 09:51:35 +0900

    fix : application.java conflict 수정
    .

--- src/main/java/com/codeit/mpl/MplApplication.java
+++ src/main/java/com/codeit/mpl/MplApplication.java
@@ -4,9 +4,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableJpaAuditing
 public class MplApplication {
 
   public static void main(String[] args) {
