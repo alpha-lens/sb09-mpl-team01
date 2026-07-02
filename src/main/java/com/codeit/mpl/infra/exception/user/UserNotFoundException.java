b1d63bdcec81e22c3cf6655daf25ef15cc731046
Alpha <mathematic026@gmail.com>
2026-07-02 15:51:05 +0900

    Refactor: UserNotFoundException에서 MplException 상속받도록 처리

--- src/main/java/com/codeit/mpl/infra/exception/user/UserNotFoundException.java
+++ src/main/java/com/codeit/mpl/infra/exception/user/UserNotFoundException.java
@@ -1,5 +1,10 @@
 package com.codeit.mpl.infra.exception.user;
 
-public class UserNotFoundException {
+import com.codeit.mpl.infra.exception.ErrorCode;
+import com.codeit.mpl.infra.exception.MplException;
 
+public class UserNotFoundException extends MplException {
+  public UserNotFoundException() {
+    super(ErrorCode.USER_NOT_FOUND);
+  }
 }
