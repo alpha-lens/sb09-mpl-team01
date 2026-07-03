543c5a05db658808d55a7a3ae58cf93e028b3c47
Alpha <mathematic026@gmail.com>
2026-07-03 19:25:44 +0900

    Refactor: WatchingSessionRepository.java에 벌크 쿼리 메서드 추가

--- src/main/java/com/codeit/mpl/domain/content/repository/WatchingSessionRepository.java
+++ src/main/java/com/codeit/mpl/domain/content/repository/WatchingSessionRepository.java
@@ -2,10 +2,16 @@ package com.codeit.mpl.domain.content.repository;
 
 import com.codeit.mpl.domain.content.entity.Content;
 import com.codeit.mpl.domain.content.entity.WatchingSession;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WatchingSessionRepository extends JpaRepository<WatchingSession, UUID> {
 
     long countByContent(Content content);
+
+    @Query("SELECT w.content.id, COUNT(w) FROM WatchingSession w WHERE w.content.id IN :contentIds GROUP BY w.content.id")
+    List<Object[]> findWatcherCountsByContentIds(@Param("contentIds") List<UUID> contentIds);
 }
