a7f990814a446cfef2dc32756ff035de5a641ad7
Alpha <mathematic026@gmail.com>
2026-07-07 20:15:42 +0900

    Refactor: 누락된 import 추가

--- src/main/java/com/codeit/mpl/domain/content/repository/WatchingSessionRepository.java
+++ src/main/java/com/codeit/mpl/domain/content/repository/WatchingSessionRepository.java
@@ -2,6 +2,7 @@ package com.codeit.mpl.domain.content.repository;
 
 import com.codeit.mpl.domain.content.entity.Content;
 import com.codeit.mpl.domain.content.entity.WatchingSession;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.UUID;
@@ -11,6 +12,6 @@ public interface WatchingSessionRepository extends JpaRepository<WatchingSession
 
     long countByContent(Content content);
 
-    @org.springframework.data.jpa.repository.Query("SELECT ws.content.id, COUNT(ws) FROM WatchingSession ws WHERE ws.content.id IN :contentIds GROUP BY ws.content.id")
-    List<Object[]> findWatcherCountsByContentIds(@Param("contentIds") java.util.List<UUID> contentIds);
+    @Query("SELECT ws.content.id, COUNT(ws) FROM WatchingSession ws WHERE ws.content.id IN :contentIds GROUP BY ws.content.id")
+    List<Object[]> findWatcherCountsByContentIds(@Param("contentIds") List<UUID> contentIds);
 }
