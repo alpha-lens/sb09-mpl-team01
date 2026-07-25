9af8dc49767af3d0573ea66b92008627891382d4
alpha_lens <mathematic026@gmail.com>
2026-07-25 11:28:21 +0900

    Fix: 알림 삭제 캐싱 오류 수정

--- src/main/java/com/codeit/mpl/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/mpl/domain/notification/repository/NotificationRepository.java
@@ -5,6 +5,7 @@ import com.codeit.mpl.domain.notification.entity.NotificationType;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -17,5 +18,6 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Notification> findByReceiverIdAndTypeAndTargetIdAndIsReadFalse(UUID receiverId, NotificationType type, UUID targetId);
 
+    @Modifying(clearAutomatically = true)
     void deleteByReceiverIdAndTypeAndTargetIdAndIsReadFalse(UUID receiverId, NotificationType type, UUID targetId);
 }
